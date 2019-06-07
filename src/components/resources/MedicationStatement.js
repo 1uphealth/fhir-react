fb17184f3904fef72f1b00633f04f8780baaa6a2
vikas1uphealth <vikas@1up.health>
2019-06-07 13:50:27 -0400

    added conditions #2

--- src/components/resources/MedicationStatement.js
+++ src/components/resources/MedicationStatement.js
@@ -37,12 +37,16 @@ class MedicationStatement extends React.Component {
         <ResourceContainer fhirResource={this.props.fhirResource} >
           <div > {/*{this.props.fhirResource.contained[0].code.coding[0].display}*/}
               {this.props.fhirResource.contained ? this.props.fhirResource.contained.map(function(medication){
-                return <MedicationDetails medication= {_.get(medication.code.coding[0], 'display')} expiration={_.get(medication.package.batch[0], 'expirationDate')}/>
+                if(medication.code && medication.code.coding && medication.code.coding.length > 0) {
+                  return <MedicationDetails medication= {_.get(medication.code.coding[0], 'display')} expiration={_.get(medication.package.batch[0], 'expirationDate')}/>
+                }
               }) : ''}
           </div>
           <div>
             {this.props.fhirResource.reasonCode ? this.props.fhirResource.reasonCode.map(function(reasonCode){
+              if(reasonCode.coding && reasonCode.coding.length > 0)  {
                 return (<p> <b>Reason:</b> {_.get(reasonCode.coding[0], 'display')}</p>)
+              }
             }) : ''}
           </div>
           <div>
@@ -50,8 +54,8 @@ class MedicationStatement extends React.Component {
                 return (
                   <div>
                     <p> <b>Dosage Instruction:</b> {_.get(dosage, 'text')} </p>
-                    <p> <b>Additional Instruction:</b> {_.get(dosage.additionalInstruction[0], 'text')} </p>
-                    <p> <b>Route:</b> {_.get(dosage.route.coding[0], 'display')} </p>
+                    {dosage.additionalInstruction[0] ? <p> <b>Additional Instruction:</b> {_.get(dosage.additionalInstruction[0], 'text')} </p> : ''}
+                    {dosage.route.coding[0] ? <p> <b>Route:</b> {_.get(dosage.route.coding[0], 'display')} </p> : ''}
                 </div>)
             }) : ''}
           </div>

00f10d19cdb5c9a8374d6cb37afafb9345bbdd92
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2019-12-04 11:47:18 +0100

    refactor folder structure

--- src/components/resources/Observation/Observation.test.js
+++ src/components/resources/Observation/Observation.test.js
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import Observation from '../Observation';
+import Observation from './Observation';
 
 test('shows the children when the checkbox is checked', () => {
   const mockResource = {

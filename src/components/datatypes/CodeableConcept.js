37fee37f241892bff318048fa2713de10a58b310
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2019-12-16 11:47:57 +0100

    add support BodySite data for Conditino component

---
+++ src/components/datatypes/CodeableConcept.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _get from 'lodash/get';
+import Coding from './Coding';
+
+const CodeableConcept = props => {
+  const { fhirData } = props;
+  if (!Array.isArray(fhirData)) {
+    return null;
+  }
+  return fhirData.map((item, i) => {
+    const codingData = _get(item, 'coding.0');
+    return <Coding key={`item-${i}`} fhirData={codingData} />;
+  });
+};
+
+CodeableConcept.propTypes = {
+  fhirData: PropTypes.shape({}).isRequired,
+};
+
+export default CodeableConcept;

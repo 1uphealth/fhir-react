6e430942c67d345be0176c09e38dfb985206a070
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2019-12-23 17:28:07 +0100

    add STU3 support to DiagnosticReport component

--- src/components/resources/DiagnosticReport/DiagnosticReport.test.js
+++ src/components/resources/DiagnosticReport/DiagnosticReport.test.js
@@ -3,11 +3,13 @@ import { render } from '@testing-library/react';
 import DiagnosticReport from './DiagnosticReport';
 
 import exampleDiagnosticReportDSTU2 from '../../../fixtures/dstu2/resources/diagnosticReport/example1.json';
+import exampleDiagnosticReportSTU3 from '../../../fixtures/stu3/resources/diagnosticReport/example1.json';
 
 describe('should render component correctly', () => {
-  it('DSTU2 - without severity field', () => {
+  it('should render with DSTU2 source data', () => {
     const defaultProps = {
       fhirResource: exampleDiagnosticReportDSTU2,
+      fhirVersion: 'dstu2',
     };
     const { getByTestId } = render(<DiagnosticReport {...defaultProps} />);
 
@@ -19,6 +21,26 @@ describe('should render component correctly', () => {
     );
 
     expect(getByTestId('categoryCoding').textContent).toContain('Haematology');
+
+    expect(getByTestId('performer').textContent).toContain(
+      'University Medical ',
+    );
+  });
+
+  it('should render with STU3 source data', () => {
+    const defaultProps = {
+      fhirResource: exampleDiagnosticReportSTU3,
+      fhirVersion: 'stu3',
+    };
+    const { getByTestId } = render(<DiagnosticReport {...defaultProps} />);
+
+    expect(getByTestId('title').textContent).toContain(
+      'blood count (hemogram)',
+    );
+
+    expect(getByTestId('categoryCoding').textContent).toContain(
+      'Haematology test',
+    );
 
     expect(getByTestId('performer').textContent).toContain(
       'University Medical ',

8ca276bf9b9b8b15e53bb83f90c4f527b79f8e34
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-01-03 13:36:49 +0100

    add STU3 support to MedicationDispense component

--- src/components/resources/MedicationDispense/MedicationDispense.js
+++ src/components/resources/MedicationDispense/MedicationDispense.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import _get from 'lodash/get';
 import Coding from '../../datatypes/Coding';
 import Reference from '../../datatypes/Reference';
+import UnhandledResourceDataStructure from '../UnhandledResourceDataStructure';
+import fhirTypes from '../fhirResourceTypes';
 
 const DosageInstruction = props => {
   const empty = '---';
@@ -22,40 +24,119 @@ const DosageInstruction = props => {
   );
 };
 
-const prepareDosageInstructionData = rawData => {
-  return rawData.map(item => {
-    const data = {};
-    data.route = _get(item, 'route.coding.0.display');
-    if (_get(item, 'doseQuantity')) {
-      data.doseQuantity = `${_get(item, 'doseQuantity.value')} ${_get(
-        item,
-        'doseQuantity.unit',
-      )}`;
-    }
-    if (_get(item, 'additionalInstructions')) {
-      data.additionalInstructions = _get(
-        item,
-        'additionalInstructions.coding.0.display',
-        '',
-      );
-    }
-    const timingRepeat = _get(item, 'timing.repeat');
-    if (timingRepeat) {
-      data.timing = `${timingRepeat.period} / ${timingRepeat.periodUnits}`;
-    }
+const commonDTO = fhirResource => {
+  const typeCoding = _get(fhirResource, 'type.coding.0');
 
-    return data;
-  });
+  return {
+    typeCoding,
+  };
 };
 
-const MedicationDispense = props => {
-  const { fhirResource } = props;
+const dstu2DTO = fhirResource => {
+  const prepareDosageInstructionData = rawData => {
+    return rawData.map(item => {
+      const data = {};
+      data.route = _get(item, 'route.coding.0.display');
+      if (_get(item, 'doseQuantity')) {
+        data.doseQuantity = `${_get(item, 'doseQuantity.value')} ${_get(
+          item,
+          'doseQuantity.unit',
+        )}`;
+      }
+      if (_get(item, 'additionalInstructions')) {
+        data.additionalInstructions = _get(
+          item,
+          'additionalInstructions.coding.0.display',
+          '',
+        );
+      }
+      const timingRepeat = _get(item, 'timing.repeat');
+      if (timingRepeat) {
+        data.timing = `${timingRepeat.period} / ${timingRepeat.periodUnits}`;
+      }
+
+      return data;
+    });
+  };
   const medicationReference = _get(fhirResource, 'medicationReference');
-  const typeCoding = _get(fhirResource, 'type.coding.0');
   const dosageInstruction = _get(fhirResource, 'dosageInstruction');
   const hasDosageInstruction =
     Array.isArray(dosageInstruction) && dosageInstruction.length > 0;
   const dosageInstructionData = prepareDosageInstructionData(dosageInstruction);
+  return { hasDosageInstruction, dosageInstructionData, medicationReference };
+};
+
+const stu3DTO = fhirResource => {
+  const prepareDosageInstructionData = rawData => {
+    return rawData.map(item => {
+      const data = {};
+      data.route = _get(item, 'route.coding.0.display');
+      if (_get(item, 'doseQuantity')) {
+        data.doseQuantity = `${_get(item, 'doseQuantity.value')} ${_get(
+          item,
+          'doseQuantity.unit',
+        )}`;
+      }
+      if (_get(item, 'additionalInstruction')) {
+        data.additionalInstructions = _get(
+          item,
+          'additionalInstruction.0.coding.0.display',
+          '',
+        );
+      }
+      const timingRepeat = _get(item, 'timing.repeat');
+      if (timingRepeat) {
+        data.timing = `${timingRepeat.period} / ${timingRepeat.periodUnit}`;
+      }
+
+      return data;
+    });
+  };
+  const medicationReference = _get(fhirResource, 'contained.0.code.coding.0');
+  const dosageInstruction = _get(fhirResource, 'dosageInstruction');
+  const hasDosageInstruction =
+    Array.isArray(dosageInstruction) && dosageInstruction.length > 0;
+  const dosageInstructionData = prepareDosageInstructionData(dosageInstruction);
+  return { medicationReference, hasDosageInstruction, dosageInstructionData };
+};
+
+const resourceDTO = (fhirVersion, fhirResource) => {
+  switch (fhirVersion) {
+    case fhirTypes.DSTU2: {
+      return {
+        ...commonDTO(fhirResource),
+        ...dstu2DTO(fhirResource),
+      };
+    }
+    case fhirTypes.STU3: {
+      return {
+        ...commonDTO(fhirResource),
+        ...stu3DTO(fhirResource),
+      };
+    }
+
+    default:
+      throw Error('Unrecognized the fhir version property type.');
+  }
+};
+
+const MedicationDispense = props => {
+  const { fhirResource, fhirVersion } = props;
+  let fhirResourceData = {};
+  try {
+    fhirResourceData = resourceDTO(fhirVersion, fhirResource);
+  } catch (error) {
+    console.warn(error.message);
+    return <UnhandledResourceDataStructure resourceName="MedicationDispense" />;
+  }
+
+  const {
+    medicationReference,
+    typeCoding,
+    hasDosageInstruction,
+    dosageInstructionData,
+  } = fhirResourceData;
+
   return (
     <div>
       {medicationReference && (
@@ -94,6 +175,7 @@ const MedicationDispense = props => {
 
 MedicationDispense.propTypes = {
   fhirResource: PropTypes.shape({}).isRequired,
+  fhirVersion: PropTypes.oneOf(['dstu2', 'stu3']).isRequired,
 };
 
 export default MedicationDispense;
--- src/components/resources/MedicationDispense/MedicationDispense.stories.js
+++ src/components/resources/MedicationDispense/MedicationDispense.stories.js
@@ -5,6 +5,8 @@ import MedicationDispense from './MedicationDispense';
 
 import dstu2Example1 from '../../../fixtures/dstu2/resources/medicationDispense/example1.json';
 import dstu2Example2 from '../../../fixtures/dstu2/resources/medicationDispense/example2.json';
+import stu3Example1 from '../../../fixtures/stu3/resources/medicationDispense/example1.json';
+import stu3Example2 from '../../../fixtures/stu3/resources/medicationDispense/example2.json';
 
 export default {
   title: 'MedicationDispense',
@@ -12,10 +14,25 @@ export default {
 
 export const DefaultVisualizationDSTU2 = () => {
   const fhirResource = object('Resource', dstu2Example1);
-  return <MedicationDispense fhirResource={fhirResource} />;
+  return <MedicationDispense fhirResource={fhirResource} fhirVersion="dstu2" />;
 };
 
 export const Example2OfDSTU2 = () => {
   const fhirResource = object('Resource', dstu2Example2);
+  return <MedicationDispense fhirResource={fhirResource} fhirVersion="dstu2" />;
+};
+
+export const Example1OfSTU3 = () => {
+  const fhirResource = object('Resource', stu3Example1);
+  return <MedicationDispense fhirResource={fhirResource} fhirVersion="stu3" />;
+};
+
+export const Example2OfSTU3 = () => {
+  const fhirResource = object('Resource', stu3Example2);
+  return <MedicationDispense fhirResource={fhirResource} fhirVersion="stu3" />;
+};
+
+export const ExampleWithoutFhirVersionProperty = () => {
+  const fhirResource = object('Resource', stu3Example1);
   return <MedicationDispense fhirResource={fhirResource} />;
 };

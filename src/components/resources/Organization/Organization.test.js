261888de8689e07d7d85a295fcc481dc4650014a
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-01-08 08:58:07 +0100

    add test for STU3 version of srource data

--- src/components/resources/Organization/Organization.test.js
+++ src/components/resources/Organization/Organization.test.js
@@ -6,7 +6,9 @@ import Organization from './Organization';
 import dstu2Example1 from '../../../fixtures/dstu2/resources/organization/example1.json';
 import dstu2Example2 from '../../../fixtures/dstu2/resources/organization/example2.json';
 
-describe('should render Gola component properly', () => {
+import stu3Example1 from '../../../fixtures/stu3/resources/organization/example1.json';
+
+describe('should render Organization component properly', () => {
   it('should render with DSTU2 source data', () => {
     const defaultProps = {
       fhirResource: dstu2Example1,
@@ -33,5 +35,17 @@ describe('should render Gola component properly', () => {
 
     expect(getByTestId('name').textContent).toContain('Clinical Lab');
     expect(queryAllByTestId('address').length).toEqual(0);
+  });
+
+  it('should render with STU3 source data', () => {
+    const defaultProps = {
+      fhirResource: stu3Example1,
+    };
+
+    const { getByTestId } = render(<Organization {...defaultProps} />);
+
+    expect(getByTestId('name').textContent).toContain('Health Level');
+    expect(getByTestId('address').textContent).toContain('Washtenaw Avenue');
+    expect(getByTestId('contact').textContent).toContain('734-677-7777');
   });
 });

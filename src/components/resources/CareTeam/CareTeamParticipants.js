65505a236ea93e66f91bc91413122fbd97fca49b
Tomasz Pewiński <tomasz.pewinski+1uphealth@htdevelopers.com>
2020-01-09 11:17:40 +0100

    Use the UI components in CareTeam

--- src/components/resources/CareTeam/CareTeamParticipants.js
+++ src/components/resources/CareTeam/CareTeamParticipants.js
@@ -2,44 +2,27 @@ import React from 'react';
 
 import CareTeamParticipant from './CareTeamParticipant';
 
+import { ValueSection, Table, TableRow, TableHeader } from '../../ui';
+
 const CareTeamParticipants = props => {
   const { participants } = props;
   const allParticipants = participants.map((participant, i) => (
     <CareTeamParticipant key={`Participant-${i}`} participant={participant} />
   ));
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <span>
-          <small className="text-uppercase text-muted">
-            <strong>Participants</strong>
-          </small>
-        </span>
-      </div>
-      <div className="row">
-        <div className="col-sm-2">
-          <small className="text-uppercase text-muted">
-            <strong>Role</strong>
-          </small>
-        </div>
-        <div className="col-sm-2">
-          <small className="text-uppercase text-muted">
-            <strong>Name</strong>
-          </small>
-        </div>
-        <div className="col-sm-2">
-          <small className="text-uppercase text-muted">
-            <strong>Start</strong>
-          </small>
-        </div>
-        <div className="col-sm-2">
-          <small className="text-uppercase text-muted">
-            <strong>End</strong>
-          </small>
-        </div>
-      </div>
-      {allParticipants}
-    </div>
+    <ValueSection label="Participants">
+      <Table>
+        <thead>
+          <TableRow>
+            <TableHeader>Role</TableHeader>
+            <TableHeader>Name</TableHeader>
+            <TableHeader>Start</TableHeader>
+            <TableHeader>End</TableHeader>
+          </TableRow>
+        </thead>
+        <tbody>{allParticipants}</tbody>
+      </Table>
+    </ValueSection>
   );
 };
 

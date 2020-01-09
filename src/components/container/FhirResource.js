70d160ee6c3d0f125bca1b79990dbd6304d2dda7
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-01-09 09:31:40 +0100

    create MedicationAdministration component (#74)
    * add scaffold of MedicationAdministration component
    
    * add tests and stories for component
    
    * add STU3 support

--- src/components/container/FhirResource.js
+++ src/components/container/FhirResource.js
@@ -24,6 +24,7 @@ import Coverage from '../resources/Coverage';
 import MedicationDispense from '../resources/MedicationDispense';
 import Organization from '../resources/Organization';
 import MedicationRequest from '../resources/MedicationRequest';
+import MedicationAdministration from '../resources/MedicationAdministration';
 import ReferralRequest from '../resources/ReferralRequest';
 
 class ErrorBoundary extends React.Component {
@@ -184,6 +185,12 @@ class FhirResource extends React.Component {
         return (
           <ResourceContainer {...this.props}>
             <MedicationRequest {...this.props} />
+          </ResourceContainer>
+        );
+      case 'MedicationAdministration':
+        return (
+          <ResourceContainer {...this.props}>
+            <MedicationAdministration {...this.props} />
           </ResourceContainer>
         );
       case 'ReferralRequest':

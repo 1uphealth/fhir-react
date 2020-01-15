02e718888667b15b34a18483758b09264c135899
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-01-15 12:25:11 +0100

    add example of Patient resource (DSTU2 version) from eCW provider

--- src/components/resources/Patient/Patient.stories.js
+++ src/components/resources/Patient/Patient.stories.js
@@ -4,6 +4,7 @@ import { object } from '@storybook/addon-knobs';
 import Patient from './Patient';
 
 import examplePatient from '../../../fixtures/dstu2/resources/patient/example.json';
+import example2PatientDSTU2 from '../../../fixtures/dstu2/resources/patient/example2.json';
 
 import examplePatientSTU3 from '../../../fixtures/stu3/resources/patient/example.json';
 import example2PatientSTU3 from '../../../fixtures/stu3/resources/patient/example2.json';
@@ -12,6 +13,11 @@ export default { title: 'Patient' };
 
 export const DefaultVisualizationDSTU2 = () => {
   const fhirResource = object('Resource', examplePatient);
+  return <Patient fhirResource={fhirResource} />;
+};
+
+export const Example2OfDSTU2 = () => {
+  const fhirResource = object('Resource', example2PatientDSTU2);
   return <Patient fhirResource={fhirResource} />;
 };
 

9eb4284aa01bf8d5e66424888b96b95f71c6afb8
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-01-20 14:12:30 +0100

    add STU3 support to Location component

--- src/components/resources/Location/Location.stories.js
+++ src/components/resources/Location/Location.stories.js
@@ -4,6 +4,7 @@ import { object } from '@storybook/addon-knobs';
 import Location from './Location';
 
 import dstu2Example1 from '../../../fixtures/dstu2/resources/location/example1.json';
+import stu3Example1 from '../../../fixtures/stu3/resources/location/example1.json';
 
 export default {
   title: 'Location',
@@ -11,5 +12,10 @@ export default {
 
 export const DefaultVisualizationDSTU2 = () => {
   const fhirResource = object('Resource', dstu2Example1);
+  return <Location fhirResource={fhirResource} />;
+};
+
+export const ExampleOfSTU3 = () => {
+  const fhirResource = object('Resource', stu3Example1);
   return <Location fhirResource={fhirResource} />;
 };

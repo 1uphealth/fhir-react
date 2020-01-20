3d6e4c200abf420c53c4dbea7b7add39f1ce4493
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-01-20 13:50:10 +0100

    add functionality to display the system name in the Telecom component

--- src/components/datatypes/Telecom/Telecom.js
+++ src/components/datatypes/Telecom/Telecom.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import _get from 'lodash/get';
 
+import './Telecom.css';
+
 const Telecom = props => {
   const { fhirData } = props;
   if (Array.isArray(fhirData)) {
     return (
       <div className="fhir-datatype__Telecom">
-        {fhirData.map((item, i) => (
-          <div className="fhir-datatype__Telecom__item" key={`phone-${i}`}>
-            {_get(item, 'value', '')}
-          </div>
-        ))}
+        {fhirData.map((item, i) => {
+          const system = _get(item, 'system', '');
+          return (
+            <div className="fhir-datatype__Telecom__item" key={`phone-${i}`}>
+              {system && (
+                <span className="fhir-datatype__Telecom__item-label">
+                  {system}
+                </span>
+              )}
+              {_get(item, 'value', '')}
+            </div>
+          );
+        })}
       </div>
     );
   }

1e9963200dfced13c4d851c920d1f99a1d832a58
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-01-22 15:52:22 +0100

    add STU3 support to Medication component

--- src/components/resources/Medication/Medication.js
+++ src/components/resources/Medication/Medication.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _get from 'lodash/get';
+
 import {
   Root,
   Header,
@@ -10,38 +12,27 @@ import {
   Value,
   BadgeSecondary,
 } from '../../ui';
-import _get from 'lodash/get';
 import Coding from '../../datatypes/Coding';
 import Reference from '../../datatypes/Reference';
+import fhirVersions from '../fhirResourceVersions';
+import UnhandledResourceDataStructure from '../UnhandledResourceDataStructure';
+import Attachment from '../../datatypes/Attachment';
 
-const Quantity = props => (
-  <span>
-    {props.value}&nbsp;{props.code || props.unit}
-  </span>
-);
-
-const Ingredient = props => {
-  const itemDisplay = _get(props, 'item.display', '');
-  const amountNumerator = _get(props, 'amount.numerator');
-  const amountDenominator = _get(props, 'amount.denominator');
-  const amount = (
-    <span>
-      <Quantity {...amountNumerator} />/<Quantity {...amountDenominator} />
-    </span>
-  );
-  return (
-    <div>
-      <label>{itemDisplay}</label>
-      <BadgeSecondary>{amount}</BadgeSecondary>
-    </div>
-  );
-};
-
-const Medication = props => {
-  const { fhirResource } = props;
-  const title = _get(fhirResource, 'code.coding.0');
+const commonDTO = fhirResource => {
+  let title = _get(fhirResource, 'code.coding.0');
+  if (!title) {
+    title = { display: _get(fhirResource, 'code.text', '') };
+  }
   const isBrand = _get(fhirResource, 'isBrand');
   const manufacturer = _get(fhirResource, 'manufacturer');
+
+  return {
+    title,
+    isBrand,
+    manufacturer,
+  };
+};
+const dstu2DTO = fhirResource => {
   const productForm = _get(fhirResource, 'product.form.coding', []);
   const hasProductForm = Array.isArray(productForm) && productForm.length > 0;
   const productIngredient = _get(fhirResource, 'product.ingredient', []);
@@ -51,6 +42,117 @@ const Medication = props => {
   const packageCoding = _get(fhirResource, 'package.container.coding', []);
   const hasPackageCoding =
     Array.isArray(packageCoding) && packageCoding.length > 0;
+
+  return {
+    productForm,
+    hasProductForm,
+    productIngredient,
+    hasProductIngredient,
+    hasProduct,
+    packageCoding,
+    hasPackageCoding,
+  };
+};
+const stu3DTO = fhirResource => {
+  const productForm = _get(fhirResource, 'form.coding', []);
+  const hasProductForm = Array.isArray(productForm) && productForm.length > 0;
+  const productIngredient = _get(fhirResource, 'ingredient', []);
+  const hasProductIngredient =
+    Array.isArray(productIngredient) && productIngredient.length > 0;
+  const hasProduct = hasProductForm || hasProductIngredient;
+  const packageCoding = _get(fhirResource, 'package.container.coding', []);
+  const hasPackageCoding =
+    Array.isArray(packageCoding) && packageCoding.length > 0;
+  let images = _get(fhirResource, 'image', []);
+  let hasImages = true;
+  if (
+    !Array.isArray(images) ||
+    images.filter(item => !!item.url).length === 0
+  ) {
+    hasImages = false;
+  }
+  return {
+    productForm,
+    hasProductForm,
+    productIngredient,
+    hasProductIngredient,
+    hasProduct,
+    packageCoding,
+    hasPackageCoding,
+    hasImages,
+    images,
+  };
+};
+
+const resourceDTO = (fhirVersion, fhirResource) => {
+  switch (fhirVersion) {
+    case fhirVersions.DSTU2: {
+      return {
+        ...commonDTO(fhirResource),
+        ...dstu2DTO(fhirResource),
+      };
+    }
+    case fhirVersions.STU3: {
+      return {
+        ...commonDTO(fhirResource),
+        ...stu3DTO(fhirResource),
+      };
+    }
+
+    default:
+      throw Error('Unrecognized the fhir version property type.');
+  }
+};
+
+const Quantity = props => (
+  <span>
+    {props.value}&nbsp;{props.code || props.unit}
+  </span>
+);
+
+const Ingredient = props => {
+  const itemDisplay =
+    _get(props, 'item') || _get(props, 'itemCodeableConcept.coding.0', '');
+  const amountNumerator = _get(props, 'amount.numerator');
+  const amountDenominator = _get(props, 'amount.denominator');
+  const amount = (
+    <span>
+      <Quantity {...amountNumerator} />/<Quantity {...amountDenominator} />
+    </span>
+  );
+  return (
+    <div>
+      <label>
+        <Coding fhirData={itemDisplay} />
+      </label>
+      <BadgeSecondary>{amount}</BadgeSecondary>
+    </div>
+  );
+};
+
+const Medication = props => {
+  const { fhirResource, fhirVersion } = props;
+  let fhirResourceData = {};
+  try {
+    fhirResourceData = resourceDTO(fhirVersion, fhirResource);
+  } catch (error) {
+    console.warn(error.message);
+    return <UnhandledResourceDataStructure resourceName="Medication" />;
+  }
+  const {
+    title,
+    isBrand,
+    manufacturer,
+    productForm,
+    productIngredient,
+    hasProductIngredient,
+    hasProduct,
+    packageCoding,
+    hasPackageCoding,
+    hasImages,
+    images,
+  } = fhirResourceData;
+
   return (
     <Root name="Medication">
       <Header>
@@ -88,6 +190,13 @@ const Medication = props => {
                 ))}
               </Value>
             )}
+            {hasImages && (
+              <Value label="Images" data-testid="product-images">
+                {images.map((item, i) => (
+                  <Attachment key={`item-${i}`} fhirData={item} />
+                ))}
+              </Value>
+            )}
           </ValueSection>
         )}
       </Body>
@@ -97,6 +206,8 @@ const Medication = props => {
 
 Medication.propTypes = {
   fhirResource: PropTypes.shape({}).isRequired,
+  fhirVersion: PropTypes.oneOf([fhirVersions.DSTU2, fhirVersions.STU3])
+    .isRequired,
 };
 
 export default Medication;
--- src/components/resources/Medication/Medication.stories.js
+++ src/components/resources/Medication/Medication.stories.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { object } from '@storybook/addon-knobs';
 
 import Medication from './Medication';
+import fhirVersions from '../fhirResourceVersions';
 
 import dstu2Example1 from '../../../fixtures/dstu2/resources/medication/example1.json';
 import dstu2Example2 from '../../../fixtures/dstu2/resources/medication/example2.json';
+import stu3Example1 from '../../../fixtures/stu3/resources/medication/example1.json';
+import stu3Example2 from '../../../fixtures/stu3/resources/medication/example2.json';
 
 export default {
   title: 'Medication',
@@ -12,10 +15,28 @@ export default {
 
 export const DefaultVisualizationDSTU2 = () => {
   const fhirResource = object('Resource', dstu2Example1);
-  return <Medication fhirResource={fhirResource} />;
+  return (
+    <Medication fhirVersion={fhirVersions.DSTU2} fhirResource={fhirResource} />
+  );
 };
 
 export const Example2OfDSTU2 = () => {
   const fhirResource = object('Resource', dstu2Example2);
-  return <Medication fhirResource={fhirResource} />;
+  return (
+    <Medication fhirVersion={fhirVersions.DSTU2} fhirResource={fhirResource} />
+  );
+};
+
+export const Example1OfSTU3 = () => {
+  const fhirResource = object('Resource', stu3Example1);
+  return (
+    <Medication fhirVersion={fhirVersions.STU3} fhirResource={fhirResource} />
+  );
+};
+
+export const Example2OfSTU3 = () => {
+  const fhirResource = object('Resource', stu3Example2);
+  return (
+    <Medication fhirVersion={fhirVersions.STU3} fhirResource={fhirResource} />
+  );
 };

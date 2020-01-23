0d3a004ce16a8568cad1fe901d650bb7a27cda44
Tomasz Pewiński <tomasz.pewinski+1uphealth@htdevelopers.com>
2020-01-23 13:29:01 +0100

    Render cost, benefit and payment details on Claim

--- src/components/resources/ClaimResponse/ClaimResponse.js
+++ src/components/resources/ClaimResponse/ClaimResponse.js
@@ -3,31 +3,57 @@ import PropTypes from 'prop-types';
 import _get from 'lodash/get';
 
 import fhirVersions from '../fhirResourceVersions';
+import Coding from '../../datatypes/Coding';
 import DateType from '../../datatypes/Date';
+import Identifier from '../../datatypes/Identifier';
+import Money from '../../datatypes/Money';
 import Reference from '../../datatypes/Reference';
 import UnhandledResourceDataStructure from '../UnhandledResourceDataStructure';
 
-import { Root, Body, Header, Title, Badge, Value } from '../../ui';
+import {
+  Badge,
+  Body,
+  Header,
+  MissingValue,
+  Root,
+  Title,
+  Value,
+  ValueSection,
+} from '../../ui';
 
 const commonDTO = fhirResource => {
   const id = _get(fhirResource, 'id');
   const created = _get(fhirResource, 'created');
   const disposition = _get(fhirResource, 'disposition');
   const requestReference = _get(fhirResource, 'request');
+  const totalCost = _get(fhirResource, 'totalCost');
+  const totalBenefit = _get(fhirResource, 'totalBenefit');
 
   return {
     id,
     created,
     disposition,
     requestReference,
+    totalCost,
+    totalBenefit,
   };
 };
 
 const dstu2DTO = fhirResource => {
   const outcome = _get(fhirResource, 'outcome');
+  const paymentTypeCoding = null;
+  const paymentAmount = _get(fhirResource, 'paymentAmount');
+  const paymentDate = _get(fhirResource, 'paymentDate');
+  const paymentRef = _get(fhirResource, 'paymentRef');
 
   return {
     outcome,
+    payment: {
+      typeCoding: paymentTypeCoding,
+      amount: paymentAmount,
+      date: paymentDate,
+      ref: paymentRef,
+    },
   };
 };
 
@@ -35,9 +61,19 @@ const stu3DTO = fhirResource => {
   const outcome =
     _get(fhirResource, 'outcome.coding[0].display') ||
     _get(fhirResource, 'outcome.coding[0].code');
+  const paymentTypeCoding = _get(fhirResource, 'payment.type.coding[0]');
+  const paymentAmount = _get(fhirResource, 'payment.amount');
+  const paymentDate = _get(fhirResource, 'payment.date');
+  const paymentRef = _get(fhirResource, 'payment.identifier');
 
   return {
     outcome,
+    payment: {
+      typeCoding: paymentTypeCoding,
+      amount: paymentAmount,
+      date: paymentDate,
+      ref: paymentRef,
+    },
   };
 };
 
@@ -76,6 +112,9 @@ const ClaimResponse = props => {
     created,
     disposition,
     requestReference,
+    totalCost,
+    totalBenefit,
+    payment,
   } = fhirResourceData;
 
   return (
@@ -100,6 +139,46 @@ const ClaimResponse = props => {
             {disposition}
           </Value>
         )}
+        {totalCost && (
+          <Value label="Total cost" data-testid="totalCost">
+            <Money fhirData={totalCost} />
+          </Value>
+        )}
+        {totalBenefit && (
+          <Value label="Total benefit" data-testid="totalBenefit">
+            <Money fhirData={totalBenefit} />
+          </Value>
+        )}
+        <ValueSection label="Payment">
+          <Value label="Type" data-testid="payment.type">
+            {payment.typeCoding ? (
+              <Coding fhirData={payment.typeCoding} />
+            ) : (
+              <MissingValue />
+            )}
+          </Value>
+          <Value label="Amount" data-testid="payment.amount">
+            {payment.amount ? (
+              <Money fhirData={payment.amount} />
+            ) : (
+              <MissingValue />
+            )}
+          </Value>
+          <Value label="Date" data-testid="payment.date">
+            {payment.date ? (
+              <DateType fhirData={payment.date} />
+            ) : (
+              <MissingValue />
+            )}
+          </Value>
+          <Value label="Reference" data-testid="payment.ref">
+            {payment.ref ? (
+              <Identifier fhirData={payment.ref} />
+            ) : (
+              <MissingValue />
+            )}
+          </Value>
+        </ValueSection>
       </Body>
     </Root>
   );
--- src/components/resources/ClaimResponse/ClaimResponse.test.js
+++ src/components/resources/ClaimResponse/ClaimResponse.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 
 import ClaimResponse from './ClaimResponse';
 import fhirVersions from '../fhirResourceVersions';
+import { nbspRegex } from '../../../testUtils';
 import dstu2Example1 from '../../../fixtures/dstu2/resources/claimResponse/example-1.json';
 import stu3Example1 from '../../../fixtures/stu3/resources/claimResponse/example-1.json';
 
@@ -23,6 +24,18 @@ describe('should render the ClaimResponse component properly', () => {
     expect(getByTestId('request').textContent).toEqual(
       'http://www.BenefitsInc.com/fhir/oralhealthclaim/15476332402',
     );
+    expect(
+      getByTestId('totalCost').textContent.replace(nbspRegex, ' '),
+    ).toEqual('135.57 USD');
+    expect(
+      getByTestId('totalBenefit').textContent.replace(nbspRegex, ' '),
+    ).toEqual('100.47 USD');
+    expect(getByTestId('payment.type').textContent).toEqual('-');
+    expect(
+      getByTestId('payment.amount').textContent.replace(nbspRegex, ' '),
+    ).toEqual('100.47 USD');
+    expect(getByTestId('payment.date').textContent).toEqual('2014-08-31');
+    expect(getByTestId('payment.ref').textContent).toEqual('201408-2-1569478');
   });
 
   it('with STU3 source data', () => {
@@ -41,5 +54,17 @@ describe('should render the ClaimResponse component properly', () => {
     expect(getByTestId('request').textContent).toEqual(
       'http://www.BenefitsInc.com/fhir/oralhealthclaim/15476332402',
     );
+    expect(
+      getByTestId('totalCost').textContent.replace(nbspRegex, ' '),
+    ).toEqual('135.57 USD');
+    expect(
+      getByTestId('totalBenefit').textContent.replace(nbspRegex, ' '),
+    ).toEqual('100.47 USD');
+    expect(getByTestId('payment.type').textContent).toContain('complete');
+    expect(
+      getByTestId('payment.amount').textContent.replace(nbspRegex, ' '),
+    ).toEqual('100.47 USD');
+    expect(getByTestId('payment.date').textContent).toEqual('2014-08-31');
+    expect(getByTestId('payment.ref').textContent).toEqual('201408-2-1569478');
   });
 });

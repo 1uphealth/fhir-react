8facd3555bc26f91232e817fd9a35549e4745aab
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-02-11 12:59:16 +0100

    add R4 support to Coverage component  - add tests  - add stories (fixtures)

--- src/components/resources/Coverage/Coverage.js
+++ src/components/resources/Coverage/Coverage.js
@@ -7,13 +7,16 @@ import Coding from '../../datatypes/Coding';
 import UnhandledResourceDataStructure from '../UnhandledResourceDataStructure';
 import fhirVersions from '../fhirResourceVersions';
 import { Root, Header, Title, Body, Value } from '../../ui';
+import Identifier from '../../datatypes/Identifier/Identifier';
 
 const commonDTO = fhirResource => {
+  const identifier = _get(fhirResource, 'identifier.0');
   const period = _has(fhirResource, 'period');
   const coverageFrom = _get(fhirResource, 'period.start');
   const coverageTo = _get(fhirResource, 'period.end');
 
   return {
+    identifier,
     period,
     coverageFrom,
     coverageTo,
@@ -38,8 +41,38 @@ const stu3DTO = fhirResource => {
     classDescription: _get(fhirResource, 'grouping.classDisplay'),
   };
   const hasDetails = Object.values(details).filter(item => !!item).length > 0;
-  const extension = _get(fhirResource, 'extension');
+  let extension = _get(fhirResource, 'extension');
   const hasExtension = Array.isArray(extension) && extension.length > 0;
+  if (hasExtension) {
+    extension = extension.map(item => ({ coding: [item.valueCoding] }));
+  }
+  return {
+    planId,
+    issuer,
+    type,
+    details,
+    hasDetails,
+    extension,
+    hasExtension,
+  };
+};
+const r4DTO = fhirResource => {
+  const issuer = _get(fhirResource, 'payor.0');
+  const planId = _get(fhirResource, 'class.plan');
+  const type = _get(fhirResource, 'type.coding.0');
+  const details = {
+    planDescription: _get(fhirResource, 'class.value'),
+    classDescription: _get(fhirResource, 'class.type'),
+  };
+  const hasDetails = Object.values(details).filter(item => !!item).length > 0;
+  let extension = _get(fhirResource, 'class');
+  const hasExtension = Array.isArray(extension) && extension.length > 0;
+  if (hasExtension) {
+    extension = extension.map(item => ({
+      ...item,
+      coding: _get(item, 'type.coding'),
+    }));
+  }
   return {
     planId,
     issuer,
@@ -63,6 +96,12 @@ const resourceDTO = (fhirVersion, fhirResource) => {
       return {
         ...commonDTO(fhirResource),
         ...stu3DTO(fhirResource),
+      };
+    }
+    case fhirVersions.R4: {
+      return {
+        ...commonDTO(fhirResource),
+        ...r4DTO(fhirResource),
       };
     }
 
@@ -92,17 +131,32 @@ const Coverage = props => {
     hasDetails,
     extension,
     hasExtension,
+    identifier,
   } = fhirResourceData;
 
   return (
     <Root name="coverage">
       <Header>
-        <Title>Plan: {planId}</Title>
+        {identifier && (
+          <Title>
+            Coverage Identifier <Identifier fhirData={identifier} />
+          </Title>
+        )}
       </Header>
       <Body>
-        {issuer && (
+        {_get(issuer, 'reference') && (
           <Value label="Issuer" data-testid="issuer">
             <Reference fhirData={issuer} />
+          </Value>
+        )}
+        {_get(issuer, 'identifier') && (
+          <Value label="Issuer" data-testid="issuer">
+            <Identifier fhirData={issuer.identifier} />
+          </Value>
+        )}
+        {planId && (
+          <Value label="PlanId" data-testid="planId">
+            {planId}
           </Value>
         )}
         {period && (
@@ -133,9 +187,18 @@ const Coverage = props => {
         )}
         {hasExtension && (
           <Value label="Extension" data-testid="extensions">
-            {extension.map((item, i) => (
-              <Coding key={`item-${i}`} fhirData={item.valueCoding} />
-            ))}
+            {extension.map((item, i) => {
+              const coding = Array.isArray(item.coding) ? item.coding : [];
+
+              return (
+                <span key={`item-${i}`}>
+                  {item.name && <span>{item.name}</span>}
+                  {coding.map((code, j) => (
+                    <Coding key={`item-${j}`} fhirData={code} />
+                  ))}
+                </span>
+              );
+            })}
           </Value>
         )}
       </Body>
@@ -145,8 +208,11 @@ const Coverage = props => {
 
 Coverage.propTypes = {
   fhirResource: PropTypes.shape({}).isRequired,
-  fhirVersion: PropTypes.oneOf([fhirVersions.DSTU2, fhirVersions.STU3])
-    .isRequired,
+  fhirVersion: PropTypes.oneOf([
+    fhirVersions.DSTU2,
+    fhirVersions.STU3,
+    fhirVersions.R4,
+  ]).isRequired,
 };
 
 export default Coverage;
--- src/components/resources/Coverage/Coverage.stories.js
+++ src/components/resources/Coverage/Coverage.stories.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { object } from '@storybook/addon-knobs';
 
 import Coverage from './Coverage';
+import fhirVersions from '../fhirResourceVersions';
 
 import exampleCoverageDstu2 from '../../../fixtures/dstu2/resources/coverage/example1.json';
 import exampleCoverageStu3 from '../../../fixtures/stu3/resources/coverage/example1.json';
 import example2CoverageStu3 from '../../../fixtures/stu3/resources/coverage/example2.json';
-import fhirVersions from '../fhirResourceVersions';
+import exampleCoverageR4 from '../../../fixtures/r4/resources/coverage/example1.json';
+import example2CoverageR4 from '../../../fixtures/r4/resources/coverage/example2.json';
 
 export default { title: 'Coverage' };
 
@@ -29,6 +31,16 @@ export const Example2OfSTU3 = () => {
   return (
     <Coverage fhirVersion={fhirVersions.STU3} fhirResource={fhirResource} />
   );
+};
+
+export const ExampleOfR4 = () => {
+  const fhirResource = object('Resource', exampleCoverageR4);
+  return <Coverage fhirResource={fhirResource} fhirVersion={fhirVersions.R4} />;
+};
+
+export const Example2OfR4 = () => {
+  const fhirResource = object('Resource', example2CoverageR4);
+  return <Coverage fhirResource={fhirResource} fhirVersion={fhirVersions.R4} />;
 };
 
 export const ExampleWithoutFhirVersionProperty = () => {

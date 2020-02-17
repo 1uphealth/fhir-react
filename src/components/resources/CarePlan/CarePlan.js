212f73200fb8f60117fb5f2933790e3578c34685
KonradNojman <konrad.nojman@htdevelopers.com>
2020-02-17 08:08:55 +0100

    Add R4 support to CarePlan component (#160)
    * Add fixtures, stories and R4 support

--- src/components/resources/CarePlan/CarePlan.js
+++ src/components/resources/CarePlan/CarePlan.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _get from 'lodash/get';
 import Reference from '../../datatypes/Reference';
 import Coding from '../../datatypes/Coding';
-import _get from 'lodash/get';
+import { hasValue } from '../../datatypes/CodeableConcept';
+import DateType from '../../datatypes/Date';
+import { isNotEmptyArray } from '../../../utils';
 
-import CarePlanActivity from './CarePlanActivity';
-import UnhandledResourceDataStructure from '../UnhandledResourceDataStructure';
 import fhirVersions from '../fhirResourceVersions';
+import UnhandledResourceDataStructure from '../UnhandledResourceDataStructure';
+import CarePlanActivity from './CarePlanActivity';
 import './CarePlan.css';
 
 import {
@@ -17,6 +20,7 @@ import {
   BadgeSecondary,
   Body,
   Value,
+  MissingValue,
 } from '../../ui';
 
 const commonDTO = fhirResource => {
@@ -28,6 +32,11 @@ const commonDTO = fhirResource => {
   const hasGoals = Array.isArray(goals);
   const addresses = _get(fhirResource, 'addresses');
   const hasAddresses = Array.isArray(addresses);
+  const description = _get(fhirResource, 'description');
+  const subject = _get(fhirResource, 'subject');
+  const periodStart = _get(fhirResource, 'period.start');
+  const periodEnd = _get(fhirResource, 'period.end');
+  const author = _get(fhirResource, 'author');
 
   return {
     status,
@@ -38,6 +47,11 @@ const commonDTO = fhirResource => {
     hasGoals,
     addresses,
     hasAddresses,
+    description,
+    subject,
+    periodStart,
+    periodEnd,
+    author,
   };
 };
 
@@ -80,10 +94,46 @@ const stu3DTO = fhirResource => {
           categories,
         };
       });
-
+  const basedOn = _get(fhirResource, 'basedOn', []);
+  const partOf = _get(fhirResource, 'partOf', []);
+  const intent = _get(fhirResource, 'intent', []);
   return {
     activity,
     hasActivity,
+    basedOn,
+    partOf,
+    intent,
+  };
+};
+
+const r4DTO = fhirResource => {
+  let activity = _get(fhirResource, 'activity');
+  const hasActivity = Array.isArray(activity);
+  activity = !hasActivity
+    ? activity
+    : activity.map(item => {
+        const categories = [
+          ..._get(item, 'detail.category.coding', []),
+          ..._get(item, 'detail.code.coding', []),
+        ];
+        return {
+          title:
+            _get(item, 'detail.code.text') ||
+            _get(item, 'detail.code.coding[0].code'),
+          hasCategories: Array.isArray(categories) && categories.length > 0,
+          categories,
+        };
+      });
+
+  const basedOn = _get(fhirResource, 'basedOn', []);
+  const partOf = _get(fhirResource, 'partOf', []);
+  const intent = _get(fhirResource, 'intent', []);
+  return {
+    activity,
+    hasActivity,
+    basedOn,
+    partOf,
+    intent,
   };
 };
 
@@ -99,6 +149,12 @@ const resourceDTO = (fhirVersion, fhirResource) => {
       return {
         ...commonDTO(fhirResource),
         ...stu3DTO(fhirResource),
+      };
+    }
+    case fhirVersions.R4: {
+      return {
+        ...commonDTO(fhirResource),
+        ...r4DTO(fhirResource),
       };
     }
 
@@ -128,8 +184,17 @@ const CarePlan = props => {
     hasAddresses,
     activity,
     hasActivity,
+    basedOn,
+    partOf,
+    intent,
+    description,
+    subject,
+    periodStart,
+    periodEnd,
+    author,
   } = fhirResourceData;
 
+  const authorArr = Array.isArray(author) ? author : [author];
   return (
     <Root name="CarePlan">
       <Header>
@@ -147,6 +212,11 @@ const CarePlan = props => {
             )}
           </Value>
         )}
+        {intent && (
+          <Value label="Intent" data-testid="intent">
+            {intent}
+          </Value>
+        )}
         {hasGoals && (
           <Value label="Goals" data-testid="goals">
             {goals.map((goal, i) => (
@@ -156,12 +226,57 @@ const CarePlan = props => {
             ))}
           </Value>
         )}
+        {description && (
+          <Value label="Description" data-testid="description">
+            {description}
+          </Value>
+        )}
+        {subject && (
+          <Value label="Subject" data-testid="subject">
+            <Reference fhirData={subject} />
+          </Value>
+        )}
+        {hasValue(author) && (
+          <Value label="Author" data-testid="author">
+            {authorArr.map((item, i) => (
+              <Reference key={`author-${i}`} fhirData={item} />
+            ))}
+          </Value>
+        )}
+        {periodStart && (
+          <Value label="Plan Period Start" data-testid="periodStart">
+            {periodStart ? (
+              <DateType fhirData={periodStart} />
+            ) : (
+              <MissingValue />
+            )}
+          </Value>
+        )}
+        {periodEnd && (
+          <Value label="Plan Period End" data-testid="periodEnd">
+            {periodEnd ? <DateType fhirData={periodEnd} /> : <MissingValue />}
+          </Value>
+        )}
         {hasAddresses && (
           <Value label="Addresses" data-testid="addresses">
             {addresses.map((address, i) => (
               <div key={`item-${i}`}>
                 <Reference fhirData={address} />
               </div>
+            ))}
+          </Value>
+        )}
+        {isNotEmptyArray(basedOn) && (
+          <Value label="Based on" data-testid="basedOn">
+            {basedOn.map((item, i) => (
+              <Reference key={`based-on-${i}`} fhirData={item} />
+            ))}
+          </Value>
+        )}
+        {isNotEmptyArray(partOf) && (
+          <Value label="Part of" data-testid="partOf">
+            {partOf.map((item, i) => (
+              <Reference key={`part-of-${i}`} fhirData={item} />
             ))}
           </Value>
         )}
@@ -181,7 +296,11 @@ const CarePlan = props => {
 
 CarePlan.propTypes = {
   fhirResource: PropTypes.shape({}).isRequired,
-  fhirVersion: PropTypes.oneOf([fhirVersions.DSTU2, fhirVersions.STU3]),
+  fhirVersion: PropTypes.oneOf([
+    fhirVersions.DSTU2,
+    fhirVersions.STU3,
+    fhirVersions.R4,
+  ]),
 };
 
 export default CarePlan;
--- src/components/resources/CarePlan/CarePlan.test.js
+++ src/components/resources/CarePlan/CarePlan.test.js
@@ -5,6 +5,8 @@ import fhirVersions from '../fhirResourceVersions';
 
 import examplecarePlanDSTU2 from '../../../fixtures/dstu2/resources/carePlan/example1.json';
 import exampleCarePlanSTU3 from '../../../fixtures/stu3/resources/carePlan/example1.json';
+import example1CarePlanR4 from '../../../fixtures/r4/resources/carePlan/weightLossPlan.json';
+import example2CarePlanR4 from '../../../fixtures/r4/resources/carePlan/pregnancyPlan.json';
 
 describe('should render component correctly', () => {
   it('should render with DSTU2 source data', () => {
@@ -20,6 +22,7 @@ describe('should render component correctly', () => {
     expect(getByTestId('category').textContent).toContain('Longitudinal');
     expect(getByTestId('addresses').textContent).toContain('Dog bite');
     expect(getByTestId('activity').textContent).toContain('ADULT DIET');
+    expect(getByTestId('subject').textContent).toEqual('Patient/d47f763e7c7f');
   });
 
   it('should render with STU3 source data', () => {
@@ -34,5 +37,59 @@ describe('should render component correctly', () => {
     expect(getByTestId('status').textContent).toContain('active');
     expect(getByTestId('addresses').textContent).toContain('obesity');
     expect(getByTestId('activity').textContent).toContain('3141-9');
+    expect(getByTestId('intent').textContent).toEqual('plan');
+    expect(getByTestId('description').textContent).toContain('Manage obesity');
+    expect(getByTestId('subject').textContent).toContain('Peter James');
+    expect(getByTestId('subject').textContent).toContain('Patient/example');
+    expect(getByTestId('author').textContent).toContain('Dr Adam Careful');
+    expect(getByTestId('periodEnd').textContent).toEqual('2017-06-01');
+    expect(getByTestId('basedOn').textContent).toEqual(
+      'Management of Type 2 Diabetes',
+    );
+    expect(getByTestId('partOf').textContent).toEqual('Overall wellness plan');
+  });
+
+  it('should render with R4 source data', () => {
+    const defaultProps = {
+      fhirResource: example1CarePlanR4,
+      fhirVersion: fhirVersions.R4,
+    };
+    const { container, getByTestId } = render(<CarePlan {...defaultProps} />);
+
+    expect(container).not.toBeNull();
+
+    expect(getByTestId('status').textContent).toContain('active');
+    expect(getByTestId('addresses').textContent).toContain('obesity');
+    expect(getByTestId('activity').textContent).toContain('3141-9');
+    expect(getByTestId('intent').textContent).toEqual('plan');
+    expect(getByTestId('goals').textContent).toEqual('Goal/example');
+    expect(getByTestId('description').textContent).toContain('Manage obesity');
+    expect(getByTestId('subject').textContent).toContain('Peter James');
+    expect(getByTestId('subject').textContent).toContain('Patient/example');
+    expect(getByTestId('author').textContent).toContain('Dr Adam Careful');
+    expect(getByTestId('periodEnd').textContent).toEqual('2017-06-01');
+    expect(getByTestId('basedOn').textContent).toEqual(
+      'Management of Type 2 Diabetes',
+    );
+    expect(getByTestId('partOf').textContent).toEqual('Overall wellness plan');
+  });
+
+  it('should render with R4 example2 data', () => {
+    const defaultProps = {
+      fhirResource: example2CarePlanR4,
+      fhirVersion: fhirVersions.R4,
+    };
+    const { container, getByTestId } = render(<CarePlan {...defaultProps} />);
+
+    expect(container).not.toBeNull();
+
+    expect(getByTestId('addresses').textContent).toContain('pregnancy');
+    expect(getByTestId('activity').textContent).toContain('First Antenatal');
+    expect(getByTestId('intent').textContent).toEqual('plan');
+    expect(getByTestId('goals').textContent).toEqual('#goal');
+    expect(getByTestId('subject').textContent).toContain('Eve Everywoman');
+    expect(getByTestId('subject').textContent).toContain('Patient/1');
+    expect(getByTestId('periodStart').textContent).toEqual('2013-01-01');
+    expect(getByTestId('periodEnd').textContent).toEqual('2013-10-01');
   });
 });

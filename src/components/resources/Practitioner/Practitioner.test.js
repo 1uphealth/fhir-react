a133824671ac0665f86e2068751181e2a01805f4
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-02-17 14:18:12 +0100

    add R4 support to Practitioner component (#162)
    * add R4 support to Practitioner component

--- src/components/resources/Practitioner/Practitioner.test.js
+++ src/components/resources/Practitioner/Practitioner.test.js
@@ -5,6 +5,8 @@ import fhirVersions from '../fhirResourceVersions';
 
 import dstu2Example1 from '../../../fixtures/dstu2/resources/practitioner/example-1.json';
 import stu3Example1 from '../../../fixtures/stu3/resources/practitioner/example-1.json';
+import r4Example1 from '../../../fixtures/r4/resources/practitioner/example1.json';
+import r4Example2 from '../../../fixtures/r4/resources/practitioner/example3.json';
 
 describe('Practitioner should render component correctly', () => {
   it('should render component correctly with DSTU2 source data', () => {
@@ -35,6 +37,39 @@ describe('Practitioner should render component correctly', () => {
     expect(getByTestId('telecom').textContent).toContain(
       'fax3125520010phone3125236837',
     );
+  });
+
+  it('should render component correctly with R4 source data', () => {
+    const defaultProps = {
+      fhirVersion: fhirVersions.R4,
+      fhirResource: r4Example1,
+    };
+    const { getByTestId, queryByTestId } = render(
+      <Practitioner {...defaultProps} />,
+    );
+
+    expect(String(getByTestId('title').textContent).trim()).toEqual(
+      'Adam Careful',
+    );
+    expect(queryByTestId('gender')).toBeNull();
+    expect(getByTestId('address').textContent).toContain('534 Erewhon');
+    expect(queryByTestId('telecom')).toBeNull();
+  });
+
+  it('should render component correctly with R4 source data - example2', () => {
+    const defaultProps = {
+      fhirVersion: fhirVersions.R4,
+      fhirResource: r4Example2,
+    };
+    const { getByTestId } = render(<Practitioner {...defaultProps} />);
+
+    expect(String(getByTestId('title').textContent).trim()).toEqual(
+      'Pieter Voigt MD(official)',
+    );
+    expect(getByTestId('gender').textContent).toEqual('male');
+    expect(getByTestId('address').textContent).toContain('Galapagosweg 91');
+    expect(getByTestId('telecom').textContent).toContain('phone0205569336');
+    expect(getByTestId('birthDate').textContent).toContain('1979-04-29');
   });
 
   it('component without fhirVersion props', () => {

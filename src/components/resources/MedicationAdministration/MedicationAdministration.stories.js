172cdf4ba0b57ec3dce0cdfa845b2e0b8715644d
KonradNojman <konrad.nojman@htdevelopers.com>
2020-02-25 08:31:46 +0100

    Add R4 support to MedicationAdministration component

--- src/components/resources/MedicationAdministration/MedicationAdministration.stories.js
+++ src/components/resources/MedicationAdministration/MedicationAdministration.stories.js
@@ -5,6 +5,9 @@ import MedicationAdministration from './MedicationAdministration';
 
 import dstu2Example1 from '../../../fixtures/dstu2/resources/medicationAdministration/example1.json';
 import stu3Example1 from '../../../fixtures/stu3/resources/medicationAdministration/example1.json';
+import r4Example1 from '../../../fixtures/r4/resources/medicationAdministration/example1.json';
+import r4Example2 from '../../../fixtures/r4/resources/medicationAdministration/example2.json';
+import r4Example3 from '../../../fixtures/r4/resources/medicationAdministration/example3.json';
 import fhirVersions from '../fhirResourceVersions';
 
 export default {
@@ -26,6 +29,36 @@ export const ExampleOfSTU3 = () => {
   return (
     <MedicationAdministration
       fhirVersion={fhirVersions.STU3}
+      fhirResource={fhirResource}
+    />
+  );
+};
+
+export const Example1OfR4 = () => {
+  const fhirResource = object('Resource', r4Example1);
+  return (
+    <MedicationAdministration
+      fhirVersion={fhirVersions.R4}
+      fhirResource={fhirResource}
+    />
+  );
+};
+
+export const Example2OfR4 = () => {
+  const fhirResource = object('Resource', r4Example2);
+  return (
+    <MedicationAdministration
+      fhirVersion={fhirVersions.R4}
+      fhirResource={fhirResource}
+    />
+  );
+};
+
+export const Example3OfR4 = () => {
+  const fhirResource = object('Resource', r4Example3);
+  return (
+    <MedicationAdministration
+      fhirVersion={fhirVersions.R4}
       fhirResource={fhirResource}
     />
   );
--- src/components/resources/MedicationAdministration/MedicationAdministration.test.js
+++ src/components/resources/MedicationAdministration/MedicationAdministration.test.js
@@ -5,6 +5,8 @@ import MedicationAdministration from './MedicationAdministration';
 import fhirVersions from '../fhirResourceVersions';
 import dstu2Example1 from '../../../fixtures/dstu2/resources/medicationAdministration/example1.json';
 import stu3Example1 from '../../../fixtures/stu3/resources/medicationAdministration/example1.json';
+import r4Example1 from '../../../fixtures/r4/resources/medicationAdministration/example1.json';
+import r4Example2 from '../../../fixtures/r4/resources/medicationAdministration/example3.json';
 
 describe('should render MedicationAdministration component properly', () => {
   it('should render with DSTU2 source data', () => {
@@ -65,5 +67,45 @@ describe('should render MedicationAdministration component properly', () => {
     );
 
     expect(getByTestId('dosageQuantity').textContent).toEqual('500 mg');
+  });
+
+  it('should render with R4 source data', () => {
+    const defaultProps = {
+      fhirResource: r4Example1,
+      fhirVersion: fhirVersions.R4,
+    };
+
+    const { container, getByTestId, queryByTestId } = render(
+      <MedicationAdministration {...defaultProps} />,
+    );
+    expect(container).not.toBeNull();
+    expect(getByTestId('title').textContent).toContain('#med0303');
+    expect(getByTestId('status').textContent).toContain('on-hold');
+    expect(getByTestId('patient').textContent).toContain('Donald Duck');
+    expect(queryByTestId('practitioner')).toBeNull();
+    expect(getByTestId('periodTimeStart').textContent).toEqual('2015-01-15');
+    expect(getByTestId('periodTimeEnd').textContent).toEqual('2015-01-15');
+    expect(getByTestId('dosageRoute').textContent).toContain('-');
+    expect(getByTestId('dosageQuantity').textContent).toEqual('-');
+  });
+
+  it('should render with R4 source data - example 2', () => {
+    const defaultProps = {
+      fhirResource: r4Example2,
+      fhirVersion: fhirVersions.R4,
+    };
+
+    const { container, getByTestId } = render(
+      <MedicationAdministration {...defaultProps} />,
+    );
+    expect(container).not.toBeNull();
+    expect(getByTestId('title').textContent).toContain('#med0306');
+    expect(getByTestId('status').textContent).toContain('completed');
+    expect(getByTestId('patient').textContent).toContain('Donald Duck');
+    expect(getByTestId('practitioner').textContent).toContain('Patrick Pump');
+    expect(getByTestId('periodTimeStart').textContent).toEqual('2015-01-15');
+    expect(getByTestId('periodTimeEnd').textContent).toEqual('2015-01-15');
+    expect(getByTestId('dosageRoute').textContent).toContain('Oral Route');
+    expect(getByTestId('dosageQuantity').textContent).toEqual('2 TAB');
   });
 });

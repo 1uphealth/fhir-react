312c0a58f16a1e4bdb52701034c889529f3131d8
Tomasz Cieciński <tomasz.ciecinski@htdevelopers.com>
2020-03-05 10:11:21 +0100

    Add R4 support, stories, tests (#184)

--- src/components/resources/Medication/Medication.stories.js
+++ src/components/resources/Medication/Medication.stories.js
@@ -8,6 +8,9 @@ import dstu2Example1 from '../../../fixtures/dstu2/resources/medication/example1
 import dstu2Example2 from '../../../fixtures/dstu2/resources/medication/example2.json';
 import stu3Example1 from '../../../fixtures/stu3/resources/medication/example1.json';
 import stu3Example2 from '../../../fixtures/stu3/resources/medication/example2.json';
+import r4Example1 from '../../../fixtures/r4/resources/medication/example1.json';
+import r4Example2 from '../../../fixtures/r4/resources/medication/example2.json';
+import r4Example3 from '../../../fixtures/r4/resources/medication/example3.json';
 
 export default {
   title: 'Medication',
@@ -38,5 +41,24 @@ export const Example2OfSTU3 = () => {
   const fhirResource = object('Resource', stu3Example2);
   return (
     <Medication fhirVersion={fhirVersions.STU3} fhirResource={fhirResource} />
+  );
+};
+
+export const Example1OfR4 = () => {
+  const fhirResource = object('Resource', r4Example1);
+  return (
+    <Medication fhirVersion={fhirVersions.R4} fhirResource={fhirResource} />
+  );
+};
+export const Example2OfR4 = () => {
+  const fhirResource = object('Resource', r4Example2);
+  return (
+    <Medication fhirVersion={fhirVersions.R4} fhirResource={fhirResource} />
+  );
+};
+export const Example3OfR4 = () => {
+  const fhirResource = object('Resource', r4Example3);
+  return (
+    <Medication fhirVersion={fhirVersions.R4} fhirResource={fhirResource} />
   );
 };
--- src/components/resources/Medication/Medication.test.js
+++ src/components/resources/Medication/Medication.test.js
@@ -7,6 +7,8 @@ import fhirVersions from '../fhirResourceVersions';
 import dstu2Example1 from '../../../fixtures/dstu2/resources/medication/example1.json';
 import stu3Example1 from '../../../fixtures/stu3/resources/medication/example1.json';
 import stu3Example2 from '../../../fixtures/stu3/resources/medication/example2.json';
+import r4Example1 from '../../../fixtures/r4/resources/medication/example1.json';
+import r4Example2 from '../../../fixtures/r4/resources/medication/example2.json';
 
 describe('should render Medication component properly', () => {
   it('should render with DSTU2 source data', () => {
@@ -69,5 +71,47 @@ describe('should render Medication component properly', () => {
     expect(container).not.toBeNull();
 
     expect(getByTestId('title').textContent).toContain('Amoxicillin');
+  });
+
+  it('should render with R4 source data', () => {
+    const defaultProps = {
+      fhirResource: r4Example1,
+      fhirVersion: fhirVersions.R4,
+    };
+
+    const { container, getByTestId, queryByTestId } = render(
+      <Medication {...defaultProps} />,
+    );
+    expect(container).not.toBeNull();
+
+    expect(getByTestId('title').textContent).toContain('Amoxicillin');
+    expect(queryByTestId('manufacturer')).toBeNull();
+    expect(queryByTestId('product-form')).toBeNull();
+    expect(queryByTestId('product-ingredient')).toBeNull();
+    expect(queryByTestId('package-container')).toBeNull();
+    expect(queryByTestId('product-images')).toBeNull();
+  });
+
+  it('should render with R4 source data - example 2', () => {
+    const defaultProps = {
+      fhirResource: r4Example2,
+      fhirVersion: fhirVersions.R4,
+    };
+
+    const { container, getByTestId, queryByTestId } = render(
+      <Medication {...defaultProps} />,
+    );
+    expect(container).not.toBeNull();
+
+    expect(getByTestId('title').textContent).toContain(
+      'Capecitabine 500mg oral tablet',
+    );
+    expect(getByTestId('manufacturer').textContent).toEqual('#org2');
+    expect(getByTestId('product-form').textContent).toContain(
+      'Tablet dose form',
+    );
+    expect(queryByTestId('package-container')).toBeNull();
+    expect(queryByTestId('product-images')).toBeNull();
+    expect(getByTestId('product-ingredient').textContent).toContain('#sub04');
   });
 });

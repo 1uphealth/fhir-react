df6daa66631f9cda63807c0cbaaa6cce9e6a5dd9
pslawinski-htd <piotr.slawinski@htdevelopers.com>
2021-09-30 15:27:15 +0200

    Address YYYY and YYYY-MM datetime datatypes

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,21 @@
 export const formatDate = (date, locale) => {
   const rawDate = new Date(date);
-  const usDate = rawDate.toLocaleDateString(locale);
+  const usDate = rawDate.toLocaleDateString(locale, whichOptions(date));
   return usDate;
+};
+
+const whichOptions = date => {
+  const YEAR_FORMAT = 'YYYY';
+  const YEAR_MONTH_FORMAT = 'YYYY-MM';
+
+  if (date.length === YEAR_FORMAT.length) {
+    return { year: 'numeric' };
+  }
+  if (date.length <= YEAR_MONTH_FORMAT.length) {
+    return {
+      year: 'numeric',
+      month: 'long',
+    };
+  }
+  return;
 };

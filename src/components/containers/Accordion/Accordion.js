510b9a48ec250a2f064633466d45d857e3b4a32d
btmikolajczyk <bartlomiej.mikolajczyk@htdevelopers.com>
2021-10-04 16:24:53 +0200

    add selectors, remove css file with styles in Accordion component

--- src/components/containers/Accordion/Accordion.js
+++ src/components/containers/Accordion/Accordion.js
@@ -1,29 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './Accordion.css';
-
 import React from 'react';
 
 const Accordion = props => {
   const { headerData, bodyData } = props;
   return (
-    <div className="accordion">
-      <div className="accordion-body">
-        <div className="accordion-item accordion-item-border-color">
-          <div className="accordion-header" id="flush-headingOne">
+    <div className="accordion fhir-container__Accordion">
+      <div className="accordion-body fhir-container__Accordion__body">
+        <div className="accordion-item fhir-container__Accordion__item">
+          <div
+            className="accordion-header fhir-container__Accordion__header"
+            id="flush-headingOne"
+          >
             <button
-              className="accordion-button collapsed text-dark bg-white shadow-none"
+              className="accordion-button collapsed text-dark bg-white shadow-none fhir-container__Accordion__header"
               type="button"
               data-bs-target="#collapseTarget"
               data-bs-toggle="collapse"
               aria-controls="collapseTarget"
               aria-expanded="false"
             >
-              <div className="d-flex justify-content-start">{headerData}</div>
+              <div className="d-flex justify-content-start fhir-container__Accordion__header__text">
+                {headerData}
+              </div>
             </button>
           </div>
-          <div className="accordion-collapse collapse " id="collapseTarget">
-            <div className="accordion-body">{bodyData}</div>
+          <div
+            className="accordion-collapse collapse fhir-container__Accordion__data"
+            id="collapseTarget"
+          >
+            <div className="accordion-body fhir-container__Accordion__data__text">
+              {bodyData}
+            </div>
           </div>
         </div>
       </div>

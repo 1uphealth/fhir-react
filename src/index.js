5947ff55e98617191918d236bf2d80f6856f3f07
btmikolajczyk <bartlomiej.mikolajczyk@htdevelopers.com>
2021-10-04 12:56:10 +0200

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
 import FhirResource from './components/containers/FhirResource';
 import fhirVersions from './components/resources/fhirResourceVersions';
 
-import './style.css';
+let BootstrapLibrary;
+if (typeof document !== 'undefined') {
+  BootstrapLibrary = require('bootstrap/dist/js/bootstrap.min.js').default;
+}
 
 export { FhirResource, fhirVersions };
 export * from './components/supportedFhirResourceList';

e23cb436e3363425eb4ce08b98fbc04db9a48faf
pslawinski-htd <piotr.slawinski@htdevelopers.com>
2021-10-27 08:29:05 +0200

    Refactoring name as header

--- src/components/datatypes/HumanName/HumanName.js
+++ src/components/datatypes/HumanName/HumanName.js
@@ -5,7 +5,7 @@ import _flatten from 'lodash/flatten';
 import './HumanName.css';
 
 function HumanName(props) {
-  const { fhirData, primary } = props;
+  const { fhirData, isTitle } = props;
   const givenName = _get(fhirData, 'given', []).join(', ');
   const familyName = _flatten(Array(_get(fhirData, 'family', ''))).join(', ');
   const suffix = _get(fhirData, 'suffix', []).join(' ');
@@ -15,24 +15,11 @@ function HumanName(props) {
     ? textName
     : `${givenName} ${familyName} ${suffix}`.trim();
 
-  const headerElement = primary ? (
-    <span className="fhir-datatype__HumanName__Header--primary">{header}</span>
-  ) : (
-    <span className="fhir-datatype__HumanName__Header--default">{header}</span>
-  );
-  const rootClassName = primary
-    ? ''
-    : 'fhir-datatype__HumanName__not-primary-block';
-
   return (
-    <div className={`fhir-datatype__HumanName ${rootClassName}`}>
-      {headerElement}
-      {use && (
-        <small className="fhir-datatype__HumanName__use-block">
-          {` (${use})`}
-        </small>
-      )}
-    </div>
+    <>
+      {header}
+      {!isTitle && <span>{` (${use})`}</span>}
+    </>
   );
 }
 

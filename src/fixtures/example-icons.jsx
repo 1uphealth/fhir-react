d007a6961cc5d311bf85231e93bb7d2340ccb0f1
pslawinski-htd <piotr.slawinski@htdevelopers.com>
2021-11-18 13:57:25 +0100

    Add custom header icons

--- src/fixtures/example-icons.jsx
+++ src/fixtures/example-icons.jsx
@@ -3,14 +3,161 @@ import React from 'react';
 export default {
   Condition: (
     <img
-      src={require('./dstu2/resources/condition/condition.svg')}
-      alt="header icon"
+      src={require('../assets/containers/Condition/condition.svg')}
+      alt="germ"
     />
   ),
   Immunization: (
     <img
-      src={require('./dstu2/resources/immunization/immunization.svg')}
-      alt="header icon"
+      src={require('../assets/containers/Immunization/immunization.svg')}
+      alt="syringe"
+    />
+  ),
+  Procedure: (
+    <img
+      src={require('../assets/containers/Procedure/procedure.svg')}
+      alt="block diagram"
+    />
+  ),
+  Practitioner: (
+    <img
+      src={require('../assets/containers/Practitioner/practitioner.svg')}
+      alt="doctor avatar"
+    />
+  ),
+  AllergyIntolerance: (
+    <img
+      src={require('../assets/containers/AllergyIntolerance/allergy-intolerance.svg')}
+      alt="sneezing emoji"
+    />
+  ),
+  Appointment: (
+    <img
+      src={require('../assets/containers/Appointment/appointment.svg')}
+      alt="calendar"
+    />
+  ),
+  CarePlan: (
+    <img
+      src={require('../assets/containers/CarePlan/care-plan.svg')}
+      alt="note with a heart"
+    />
+  ),
+  CareTeam: (
+    <img
+      src={require('../assets/containers/CareTeam/care-team.svg')}
+      alt="group of people"
+    />
+  ),
+  Claim: (
+    <img
+      src={require('../assets/containers/Claim/claim.svg')}
+      alt="clipboard with a symmetrical cross"
+    />
+  ),
+  ClaimResponse: (
+    <img
+      src={require('../assets/containers/ClaimResponse/claim-response.svg')}
+      alt="clipboard with a symmetrical cross and a return arrow"
+    />
+  ),
+  Device: (
+    <img
+      src={require('../assets/containers/Device/device.svg')}
+      alt="computer rack"
+    />
+  ),
+  DiagnosticReport: (
+    <img
+      src={require('../assets/containers/DiagnosticReport/diagnostic-report.svg')}
+      alt="note with shapes"
+    />
+  ),
+  DocumentReference: (
+    <img
+      src={require('../assets/containers/DocumentReference/document-reference.svg')}
+      alt="hand holding a note"
+    />
+  ),
+  Encounter: (
+    <img
+      src={require('../assets/containers/Encounter/encounter.svg')}
+      alt="conversation between two individuals"
+    />
+  ),
+  ExplanationOfBenefit: (
+    <img
+      src={require('../assets/containers/ExplanationOfBenefit/explanation-of-benefit.svg')}
+      alt="percentage sign"
+    />
+  ),
+  FamilyMemberHistory: (
+    <img
+      src={require('../assets/containers/FamilyMemberHistory/family-member-history.svg')}
+      alt="parent and child"
+    />
+  ),
+  Goal: (
+    <img
+      src={require('../assets/containers/Goal/goal.svg')}
+      alt="arrow hits bullseye"
+    />
+  ),
+  List: (
+    <img src={require('../assets/containers/List/list.svg')} alt="checklist" />
+  ),
+  Location: (
+    <img
+      src={require('../assets/containers/Location/location.svg')}
+      alt="location marker"
+    />
+  ),
+  Medication: (
+    <img
+      src={require('../assets/containers/Medication/medication.svg')}
+      alt="some pills"
+    />
+  ),
+  MedicationAdministration: (
+    <img
+      src={require('../assets/containers/MedicationAdministration/medication-administration.svg')}
+      alt="pill gets swallowed"
+    />
+  ),
+  MedicationKnowledge: (
+    <img
+      src={require('../assets/containers/MedicationKnowledge/medication-knowledge.svg')}
+      alt="pill database"
+    />
+  ),
+  MedicationStatement: (
+    <img
+      src={require('../assets/containers/MedicationStatement/medication-statement.svg')}
+      alt="note with symmetrical cross"
+    />
+  ),
+  Observation: (
+    <img
+      src={require('../assets/containers/Observation/observation.svg')}
+      alt="hospital bed"
+    />
+  ),
+  Questionnaire: (
+    <img
+      src={require('../assets/containers/Questionnaire/questionnaire.svg')}
+      alt="clipboard and pen"
+    />
+  ),
+  QuestionnaireResponse: (
+    <img
+      src={require('../assets/containers/QuestionnaireResponse/questionnaire-response.svg')}
+      alt="clipboard and return arrow"
+    />
+  ),
+  ResearchStudy: (
+    <img
+      src={require('../assets/containers/ResearchStudy/research-study.svg')}
+      alt="finger pointing something in a book"
     />
   ),
 };

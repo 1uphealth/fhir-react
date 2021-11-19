5024a3b99f764317db8074345b9e4ca1d56efdff
btmikolajczyk <bartlomiej.mikolajczyk@htdevelopers.com>
2021-11-19 15:29:07 +0100

    start adding observation graph

--- src/components/resources/Observation/ObservationGraph.js
+++ src/components/resources/Observation/ObservationGraph.js
@@ -1,68 +1,127 @@
+import './ObservationGraph.css';
+
 import React from 'react';
 import _get from 'lodash/get';
 import _isNumber from 'lodash/isNumber';
-import './ObservationGraph.css';
 
 const ObservationGraph = props => {
   if (props.referenceRange && _isNumber(_get(props, 'valueQuantity.value'))) {
     const tooLow = _get(props, 'referenceRange[0].low.value');
     const tooHigh = _get(props, 'referenceRange[0].high.value');
+    const edgeRange = { width: 4, color: 'rounded' };
+    const observationValue = {
+      width: 1,
+      color: 'bg-dark rounded-circle border border-1 border-light',
+    };
+    const edgeCaseValue =  {
+      width: 1,
+      color: 'bg-primary rounded border border-1 border-light',
+    },
+    const edgeCompletionValue = {
+      width: 94,
+      color: 'bg-primary rounded border border-1 border-light',
+    };
+
     if (!tooLow || !tooHigh) return null;
 
-    const actual = props.valueQuantity.value;
-    const maxNum = Math.max(tooHigh, actual);
-    const minNum = Math.min(tooLow, actual);
-    const startAt = (maxNum - minNum) * -0.5 + minNum;
-    const endAt = (maxNum - minNum) * 0.5 + maxNum;
-    const zeroShift = 0 - startAt;
-    const lowBar = Math.round(((zeroShift + tooLow) / endAt) * 100);
-    const rangeBar = Math.round(((zeroShift + tooHigh) / endAt) * 100) - lowBar;
-    const valueBar = Math.round(((zeroShift + actual) / endAt) * 100) + 3;
-    const higherBar = 100 - rangeBar - lowBar;
+    const actualValue = props.valueQuantity.value;
+    let graphRanges = [];
+
+    if (actualValue >= tooLow && actualValue <= tooHigh) {
+      if (actualValue === tooLow) {
+        graphRanges = [
+          edgeRange,
+          edgeCaseValue,
+          observationValue,
+          edgeCompletionValue,
+          edgeRange,
+        ];
+      } else if (actualValue === tooHigh) {
+        graphRanges = [
+          edgeRange,
+          edgeCompletionValue,
+          observationValue,
+          edgeCaseValue,
+          edgeRange,
+        ];
+      } else {
+        const actualRangeWidthLeft = Math.round(
+          ((actualValue - tooLow) * 100) / (tooHigh - tooLow),
+        );
+        const actualRangeWidthRight = 95 - 1 - actualRangeWidthLeft;
+        graphRanges = [
+          edgeRange,
+          {
+            width: actualRangeWidthLeft,
+            color: 'bg-primary rounded border border-1 border-light',
+          },
+          observationValue,
+          {
+            width: actualRangeWidthRight,
+            color: 'bg-primary rounded border border-1 border-light',
+          },
+          edgeRange,
+        ];
+      }
+    } else {
+      if (actualValue < tooLow) {
+        const actualRangeWidth = Math.round(
+          ((tooHigh - tooLow) * 100) / (tooHigh - actualValue),
+        );
+        graphRanges = [
+          edgeRange,
+          observationValue,
+          {
+            width: 95 - actualRangeWidth,
+            color: 'rounded',
+          },
+          {
+            width: actualRangeWidth,
+            color: 'bg-primary rounded border border-1 border-light',
+          },
+          edgeRange,
+        ];
+      } else {
+        const actualRangeWidth = Math.round(
+          ((tooHigh - tooLow) * 100) / (actualValue - tooLow),
+        );
+        graphRanges = [
+          edgeRange,
+          {
+            width: actualRangeWidth,
+            color: 'bg-primary rounded border border-1 border-light',
+          },
+          {
+            width: 95 - actualRangeWidth,
+            color: 'rounded',
+          },
+          observationValue,
+          edgeRange,
+        ];
+      }
+    }
+    console.log(tooLow, actualValue, tooHigh);
+    console.log(graphRanges);
 
     return (
       <div className="fhir-resource__ObservationGraph">
-        <div
-          className="fhir-resource__ObservationGraph__value-wrapper"
-          style={{ left: `${valueBar}%` }}
-        >
-          <code className="fhir-resource__ObservationGraph__value-actual">
-            {actual}
-          </code>
-          &nbsp;
-          <span className="fhir-resource__ObservationGraph__value-unit">
-            {props.valueQuantity.unit}
-          </span>
-        </div>
         <div className="fhir-resource__ObservationGraph__progress">
-          <div
-            className="fhir-resource__ObservationGraph__progress-bar--outside-range"
-            style={{ width: `${lowBar}%` }}
-          >
-            <small>
-              <strong data-testid="tooLow">{`< ${tooLow}`}</strong>
-            </small>
-          </div>
-          <div
-            className="fhir-resource__ObservationGraph__progress-bar--inside-range"
-            role="progressbar"
-            style={{ width: `${rangeBar}%` }}
-            aria-valuenow={actual}
-            aria-valuemin={tooLow}
-            aria-valuemax={tooHigh}
-          ></div>
-          <div
-            className="fhir-resource__ObservationGraph__progress-value"
-            style={{ left: `${valueBar}%` }}
-          ></div>
-          <div
-            className="fhir-resource__ObservationGraph__progress-bar--outside-range"
-            style={{ width: `${higherBar}%` }}
-          >
-            <small>
-              <strong data-testid="tooHigh">{`> ${tooHigh}`}</strong>
-            </small>
-          </div>
+          {graphRanges.map(range => {
+            if (range.color.includes('bg-dark')) {
+              return (
+                <div
+                  className={`${range.color} mx-1`}
+                  style={{ width: '15px' }}
+                ></div>
+              );
+            }
+            return (
+              <div
+                className={`${range.color}`}
+                style={{ width: `${range.width}%` }}
+              ></div>
+            );
+          })}
         </div>
       </div>
     );

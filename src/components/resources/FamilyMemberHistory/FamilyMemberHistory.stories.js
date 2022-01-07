65ed20032be80ba52a5e6e626637234c65d0cfd2
Sebastian Flejszman <flejszmansebastian@gmail.com>
2022-01-07 07:02:29 +0100

    [PATIENTAPP-56] Add FHIR Resource: Family Member History (#312)
    * update styles for component FamilyMemberHistory
    * fix lint

--- src/components/resources/FamilyMemberHistory/FamilyMemberHistory.stories.js
+++ src/components/resources/FamilyMemberHistory/FamilyMemberHistory.stories.js
@@ -8,6 +8,7 @@ import example1DSTU2 from '../../../fixtures/dstu2/resources/familyMemberHistory
 
 import example1STU3 from '../../../fixtures/stu3/resources/familyMemberHistory/example1.json';
 import example2STU3 from '../../../fixtures/stu3/resources/familyMemberHistory/example2.json';
+import fhirIcons from '../../../fixtures/example-icons';
 
 export default {
   title: 'FamilyMemberHistory',
@@ -19,6 +20,7 @@ export const DefaultVisualizationDSTU2 = () => {
     <FamilyMemberHistory
       fhirVersion={fhirVersions.DSTU2}
       fhirResource={fhirResource}
+      fhirIcons={fhirIcons}
     />
   );
 };
@@ -29,6 +31,7 @@ export const Example1OfSTU3 = () => {
     <FamilyMemberHistory
       fhirVersion={fhirVersions.STU3}
       fhirResource={fhirResource}
+      fhirIcons={fhirIcons}
     />
   );
 };
@@ -39,6 +42,7 @@ export const Example2OfSTU3 = () => {
     <FamilyMemberHistory
       fhirVersion={fhirVersions.STU3}
       fhirResource={fhirResource}
+      fhirIcons={fhirIcons}
     />
   );
 };

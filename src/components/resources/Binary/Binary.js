560e52a190e97123cab418be0df0102e9a24c93e
Sebastian Flejszman <flejszmansebastian@gmail.com>
2022-01-10 10:56:43 +0100

    [PATIENTAPP-50] Add FHIR Resource: Binary (#318)
    * update styles for Binary component
    
    * update Accordion condition
    
    * add Binary icon

--- src/components/resources/Binary/Binary.js
+++ src/components/resources/Binary/Binary.js
@@ -2,32 +2,43 @@ import React from 'react';
 import Img from '../../datatypes/Img';
 import Pdf from '../../datatypes/Pdf';
 import BinaryText from '../../datatypes/BinaryText';
+import Accordion from '../../containers/Accordion/Accordion';
+import { Body, Header } from '../../ui';
 
 const Binary = props => {
-  const { fhirResource } = props;
+  const { fhirResource, fhirIcons } = props;
+  const headerIcon = fhirIcons && fhirIcons['Binary'];
+
+  const loadBinaryFile = () => {
+    switch (fhirResource.contentType) {
+      case 'application/pdf':
+        if (props.children && typeof props.children === 'function') {
+          return props.children(fhirResource.content, fhirResource.contentType);
+        }
+        return <Pdf fhirResource={fhirResource} />;
+      case 'image/jpeg':
+        return <Img fhirResource={fhirResource} />;
+      case 'application/xml':
+        return <BinaryText fhirResource={fhirResource} />;
+      case 'application/json':
+        return <BinaryText fhirResource={fhirResource} />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <div>
-      {(() => {
-        switch (fhirResource.contentType) {
-          case 'application/pdf':
-            if (props.children && typeof props.children === 'function') {
-              return props.children(
-                fhirResource.content,
-                fhirResource.contentType,
-              );
-            }
-            return <Pdf fhirResource={fhirResource} />;
-          case 'image/jpeg':
-            return <Img fhirResource={fhirResource} />;
-          case 'application/xml':
-            return <BinaryText fhirResource={fhirResource} />;
-          case 'application/json':
-            return <BinaryText fhirResource={fhirResource} />;
-          default:
-            return null;
+      <Accordion
+        headerContent={
+          <Header
+            resourceName="Binary"
+            title={`Binary file: ${fhirResource.contentType}`}
+            icon={headerIcon}
+          />
         }
-      })()}
+        bodyContent={<Body>{loadBinaryFile()}</Body>}
+      />
     </div>
   );
 };

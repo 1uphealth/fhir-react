ce2be3182b76491ff5f3743756f399dd558e0e57
Daria Lasecka <daria.lasecka@htdevelopers.com>
2022-02-17 11:03:55 +0100

    Add gap in components body table data

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -128,7 +128,7 @@ export const ValueUnit = props => (
 export const Body = ({ tableData = [], reverseContent, children }) => (
   <div className="fhir-ui__Body">
     {reverseContent ? children : null}
-    <div className="row">
+    <div className="row fhir-ui__Body__row-gap">
       {tableData.map(
         (value, index) =>
           value.status && (

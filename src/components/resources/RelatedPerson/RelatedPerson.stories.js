171891580ce935ad9f5f6a8aa5a4466671811276
Daria Lasecka <daria.lasecka@htdevelopers.com>
2022-03-04 10:18:20 +0100

    Fix icon test in patient tests, add version to related person stories, add tests for related person

--- src/components/resources/RelatedPerson/RelatedPerson.stories.js
+++ src/components/resources/RelatedPerson/RelatedPerson.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { object } from '@storybook/addon-knobs';
 
 import RelatedPerson from './RelatedPerson';
+import fhirVersions from '../fhirResourceVersions';
 
 import example1RelatedPerson from '../../../fixtures/dstu2/resources/relatedPerson/example1.json';
 import example2RelatedPersonDSTU2 from '../../../fixtures/dstu2/resources/relatedPerson/example2.json';
@@ -12,10 +13,21 @@ export default { title: 'RelatedPerson' };
 
 export const DefaultVisualizationDSTU2 = () => {
   const fhirResource = object('Resource', example1RelatedPerson);
-  return <RelatedPerson fhirResource={fhirResource} fhirIcons={fhirIcons} />;
+  return (
+    <RelatedPerson
+      fhirResource={fhirResource}
+      fhirVersion={fhirVersions.DSTU2}
+      fhirIcons={fhirIcons}
+    />
+  );
 };
 
 export const Example2OfDSTU2 = () => {
   const fhirResource = object('Resource', example2RelatedPersonDSTU2);
-  return <RelatedPerson fhirResource={fhirResource} />;
+  return (
+    <RelatedPerson
+      fhirResource={fhirResource}
+      fhirVersion={fhirVersions.DSTU2}
+    />
+  );
 };

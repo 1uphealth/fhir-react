d914f9ba5534ca2e59b17aa44be40cef6fad3fc2
Daria Lasecka <daria.lasecka@htdevelopers.com>
2022-03-10 10:31:42 +0100

    Updte functions names

--- src/components/resources/RelatedPerson/RelatedPerson.stories.js
+++ src/components/resources/RelatedPerson/RelatedPerson.stories.js
@@ -4,16 +4,16 @@ import { object } from '@storybook/addon-knobs';
 import RelatedPerson from './RelatedPerson';
 import fhirVersions from '../fhirResourceVersions';
 
-import example1RelatedPersonDSTU2 from '../../../fixtures/dstu2/resources/relatedPerson/example1.json';
-import example1RelatedPersonSTU3 from '../../../fixtures/stu3/resources/relatedPerson/example1.json';
-import example1RelatedPersonR4 from '../../../fixtures/r4/resources/relatedPerson/example1.json';
+import exampleDSTU2 from '../../../fixtures/dstu2/resources/relatedPerson/example1.json';
+import exampleSTU3 from '../../../fixtures/stu3/resources/relatedPerson/example1.json';
+import exampleR4 from '../../../fixtures/r4/resources/relatedPerson/example1.json';
 
 import fhirIcons from '../../../fixtures/example-icons';
 
 export default { title: 'RelatedPerson' };
 
-export const Example1 = () => {
-  const fhirResource = object('Resource', example1RelatedPersonDSTU2);
+export const ExampleDSTU2 = () => {
+  const fhirResource = object('Resource', exampleDSTU2);
   return (
     <RelatedPerson
       fhirResource={fhirResource}
@@ -23,8 +23,8 @@ export const Example1 = () => {
   );
 };
 
-export const Example2 = () => {
-  const fhirResource = object('Resource', example1RelatedPersonSTU3);
+export const ExampleSTU3 = () => {
+  const fhirResource = object('Resource', exampleSTU3);
   return (
     <RelatedPerson
       fhirResource={fhirResource}
@@ -34,8 +34,8 @@ export const Example2 = () => {
   );
 };
 
-export const Example3 = () => {
-  const fhirResource = object('Resource', example1RelatedPersonR4);
+export const ExampleR4 = () => {
+  const fhirResource = object('Resource', exampleR4);
   return (
     <RelatedPerson
       fhirResource={fhirResource}

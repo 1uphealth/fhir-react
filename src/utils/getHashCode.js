d6b60d05fc70b496e669357221156ef2eaab0900
Daria Lasecka <daria.lasecka@htdevelopers.com>
2022-05-12 13:37:17 +0200

    Add hashCode function

---
+++ src/utils/getHashCode.js
@@ -0,0 +1,39 @@
+export const getHashCode = text => {
+  let hash = 0,
+    i,
+    chr;
+  if (text.length === 0) return hash;
+  for (i = 0; i < text.length; i++) {
+    chr = text.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+export const replacerFunc = () => {
+  const visited = new WeakSet();
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (visited.has(value)) {
+        return;
+      }
+      visited.add(value);
+    }
+    return value;
+  };
+};
+
+export const circObjToString = obj => {
+  let cache = [];
+  return JSON.stringify(obj, (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      // Duplicate reference found, discard key
+      if (cache.includes(value)) return;
+
+      // Store value in our collection
+      cache.push(value);
+    }
+    return value;
+  });
+};

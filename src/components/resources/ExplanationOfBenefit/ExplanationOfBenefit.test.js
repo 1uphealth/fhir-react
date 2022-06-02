905b4935414dcd3a81698eec00bfa54ea2002c06
Daria Lasecka <daria.lasecka@htdevelopers.com>
2022-06-02 13:44:06 +0200

    Use productOrServise if revenue is falsly

--- src/components/resources/ExplanationOfBenefit/ExplanationOfBenefit.test.js
+++ src/components/resources/ExplanationOfBenefit/ExplanationOfBenefit.test.js
@@ -178,7 +178,7 @@ describe('should render ExplanationOfBenefit component properly', () => {
     const explanationService = getAllByTestId('explanation.service').map(
       n => n.textContent,
     );
-    const expectedArray = [' ', ' '];
+    const expectedArray = ['(1205)', '(group)'];
     explanationService.forEach((x, i) => expect(x).toContain(expectedArray[i]));
 
     const explanationServicedDate = getAllByTestId(
@@ -299,7 +299,11 @@ describe('should render ExplanationOfBenefit component properly', () => {
     const explanationService = getAllByTestId('explanation.service').map(
       n => n.textContent,
     );
-    const expectedArray = [' ', ' ', ' '];
+    const expectedArray = [
+      'Encounter for symptom (185345009)',
+      'Acute bronchitis (disorder) (10509002)',
+      'Measurement of respiratory function (procedure) (23426006)',
+    ];
     const replaceWhitespaces = text => text.replace(/\s+/g, ' ');
     explanationService.forEach((x, i) => {
       expect(replaceWhitespaces(x)).toEqual(

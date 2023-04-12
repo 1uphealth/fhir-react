68dc9c16ce9b03d9086705c175f2188a23ecc7f2
Patrick Hessman <patrick.hessman@1up.health>
2023-04-12 13:31:44 -0400

    converted fhirResource and ErrorBoundary to functional components, tried using try catch approach to fix ErrorBoundary

--- src/components/containers/FhirResource/FhirResource.js
+++ src/components/containers/FhirResource/FhirResource.js
@@ -1,58 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as FhirResourceTypes from '../../supportedFhirResourceList';
 import ResourceContainer from '../ResourceContainer';
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false, error: null };
-  }
 
-  componentDidCatch(error, info) {
-    // Display fallback UI
-    this.setState({ hasError: true, error });
-    // You can also log the error to an error reporting service
-    // logErrorToMyService(error, info);
+const ErrorBoundary = props => {
+  const [hasError, setHasError] = useState(false);
+  const [error, setError] = useState();
+
+  const errCheck = () => {
+    if (hasError) {
+      // You can render any custom fallback UI
+      return (
+        <ResourceContainer>
+          <FhirResourceTypes.Generic />
+        </ResourceContainer>
+      );
+    }
+    return props.children;
+  };
+
+  try {
+    const content = errCheck();
+    return content;
+  } catch (err) {
+    setError(err);
+    setHasError(true);
     console.error(
       'An error occured when trying to render a FHIR Component:',
       error,
     );
+    const content = errCheck();
+    return content;
   }
+};
 
-  render() {
-    if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return (
-        <ResourceContainer {...this.props}>
-          <FhirResourceTypes.Generic {...this.props} />
-        </ResourceContainer>
-      );
-    }
-    return this.props.children;
-  }
-}
-
-class FhirResource extends React.Component {
-  renderSwitch() {
-    const { resourceType } = this.props.fhirResource || {};
+export const FhirResource = props => {
+  const renderSwitch = () => {
+    const { resourceType } = props.fhirResource || {};
     switch (resourceType) {
       case 'Binary':
-        return <FhirResourceTypes.Binary {...this.props} />;
+        return <FhirResourceTypes.Binary {...props} />;
       default:
         const FhirComponent =
           FhirResourceTypes[resourceType] !== undefined
             ? FhirResourceTypes[resourceType]
             : FhirResourceTypes.Generic;
         return (
-          <ResourceContainer {...this.props}>
-            <FhirComponent {...this.props} />
+          <ResourceContainer {...props}>
+            <FhirComponent {...props} />
           </ResourceContainer>
         );
     }
-  }
-
-  render() {
-    return <ErrorBoundary>{this.renderSwitch()}</ErrorBoundary>;
-  }
-}
-
-export default FhirResource;
+  };
+  return <ErrorBoundary>{renderSwitch()}</ErrorBoundary>;
+};

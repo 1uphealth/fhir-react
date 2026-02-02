9a1035ebd633798e42b3f1e855cab4788cf745e4
Tiffany Forkner <tiffany.forkner@1up.health>
2026-02-02 15:00:02 -0500

    updating babel and eslint

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     //   lines: 70,
     // },
   },
-  moduleDirectories: ['node_modules', 'src'],
+  moduleDirectories: ['node_modules'],
   moduleNameMapper: {
     '.*\\.(css|less|styl|scss|sass)$': '<rootDir>/jest/mocks/cssModule.js',
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -24,4 +24,12 @@ module.exports = {
   },
   // testRegex: 'tests/.*\\.test\\.js$',
   snapshotSerializers: [],
+  testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  transform: {
+    "\\.[jt]sx?$": ["babel-jest", { "extends": "./.babelrc", "plugins": ["babel-plugin-transform-import-meta"] }]
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@nivo|d3-.*)/)",
+  ],
 };

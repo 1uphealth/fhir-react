0d81aade7f46f800aef52c4e920ff9e64d5e7aea
Tiffany Forkner <tiffany.forkner@1up.health>
2026-02-03 09:21:33 -0500

    removed commonjs from externals

--- webpack.config.js
+++ webpack.config.js
@@ -49,8 +49,8 @@ module.exports = {
     ],
   },
   externals: {
-    react: 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-    'react-dom': 'commonjs react-dom',
+    react: 'react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+    'react-dom': 'react-dom',
   },
   plugins: [
     new MiniCssExtractPlugin({
